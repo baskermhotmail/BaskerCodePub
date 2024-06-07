c7c2e702f9636b8c900a12d25ec243d1d4548f5f
Basker M <baskerm@hotmail.com>
2024-06-06 20:20:13 -0400

    Added comments, beautified code and modified README

--- StaxPayments/List.h
+++ StaxPayments/List.h
@@ -7,11 +7,14 @@ using namespace std;
 // All template definitions must be in header files. So no .cpp file for List
 ///
 template <typename T> class ListImpl { // Implementation class
+  // All methods private
   ListImpl<T>* prev;
   ListImpl<T>* next;
   T value;
  ListImpl(const T& actualValue, ListImpl* prevImpl = nullptr, ListImpl* nextImpl = nullptr):value(actualValue),prev(prevImpl),next(nextImpl)
     {}
+
+  // No copy constructor, assignment operator, move constructor and operator
   ListImpl(const ListImpl& ref)=delete;
   ListImpl( ListImpl&& ref)=delete;
   ListImpl& operator= (const ListImpl& ref) =delete;
@@ -27,6 +30,7 @@ template <typename T> class List {
   ListImpl<T>* listImpl_ = nullptr;
   ListImpl<T>* frontImpl_=nullptr;
   ListImpl<T>* backImpl_=nullptr;
+  // No copy constructor, assignment operator, move constructor and operator
   List(const List& ref)=delete;
   List( List&& ref)=delete;
   List& operator= (const List& ref) =delete;
@@ -34,124 +38,34 @@ template <typename T> class List {
 public:
   List(){}// Default constructor
   ~List() {clear();} // Destructor
-  void push_back(const T& value) // Add to the end
-  {
-    ListImpl<T>* currentBack = backImpl_;
-    if(listImpl_) {
-      backImpl_->next = new ListImpl<T>(value,currentBack);
-      backImpl_ = backImpl_->next;
-    } else {
-      listImpl_ = new ListImpl<T>(value);
-      frontImpl_ = listImpl_;
-      backImpl_ = listImpl_;
-    }
-    size_++;
-  }
-  void push_front(const T& value) { // Add to the front
-    ListImpl<T>* currentFront = frontImpl_;
-    if(listImpl_) {
-      frontImpl_->prev = new ListImpl<T>(value,0,currentFront);
-      frontImpl_ = frontImpl_->prev;
-      listImpl_ = frontImpl_;
-    } else {
-      listImpl_ = new ListImpl<T>(value);
-      frontImpl_ = listImpl_;
-      backImpl_ = listImpl_;
-    }
-    size_++;
-  }
-  void pop_back(){ // remove last elem    
-    if( backImpl_) {
-      if( backImpl_->prev){
-	ListImpl<T>* tmpImpl = backImpl_->prev;
-	delete backImpl_;
-	backImpl_ = tmpImpl;
-	backImpl_->next = nullptr;
-	size_--;
-      } else if(size_ == 1) {
-	delete listImpl_;
-	frontImpl_ = backImpl_ = listImpl_ =nullptr;
-	size_ =0;
-      } else { // empty list. Do nothing.
-      }
-    }    
-  }
-  void pop_front(){ // remove first elem
-    
-    if( frontImpl_) {
-      if( frontImpl_->next){
-	ListImpl<T>* tmpImpl = frontImpl_->next;
-	delete frontImpl_;
-	frontImpl_ = tmpImpl;
-	frontImpl_->prev = nullptr;
-	listImpl_= frontImpl_;
-	size_--;
-      } else if(size_ == 1) {
-	delete listImpl_;
-	frontImpl_ = backImpl_ = listImpl_ =nullptr;
-	size_ =0;
-      } else { // empty list. Do nothing.
-      }
-    }    
+  void push_back(const T& value); // Add to the end
+  void push_front(const T& value); // Add to the beginning
 
-  }
+  void pop_back();
+  void pop_front();
   const T& back()const{ // Last elem value
     return backImpl_->value;
   }
   const T& front()const{ // First elem value
     return frontImpl_->value;
   }
-  void print(const char separator= ' ',std::ostream& out = std::cout)const{
-    // Printing function with a single character separator 
-    ListImpl<T> *implPtr= listImpl_;
-    if(!implPtr){
-      out << endl;
-      return;
-    }
-    out << implPtr->value ;
-    implPtr = implPtr->next;
-    while(implPtr) {
-      out << separator <<implPtr->value ;
-      implPtr = implPtr->next;
-    }
-    out << std::endl;
-  }
-  void print(const char* separator,std::ostream &out = std::cout)const{
-      // Printing function with a string separator 
-    ListImpl<T> *implPtr= listImpl_;
-    if(!implPtr){
-      out << endl;
-      return;
-    }
-    out << implPtr->value ;
-    implPtr = implPtr->next;
-    while(implPtr) {
-      out << separator <<implPtr->value ;
-      implPtr = implPtr->next;
-    }
-    out << std::endl;
-  }
+  void print(const char separator= ' ',std::ostream& out = std::cout)const;
+  
+  void print(const char* separator,std::ostream &out = std::cout)const;
   void remove(const T& t);// Remove the first element matching the value
   void remove_all(const T& t); // Remove all elements matcching the value
   unsigned size()const {return size_;} // Returns size
   bool empty() const {return size_ == 0;} // Checks emptiness
-  void clear() { // Clears out all entries.
-    ListImpl<T>* impl= listImpl_;
-    size_ = 0;
-    frontImpl_ = backImpl_ = listImpl_ = nullptr;
-    while(impl) {
-      ListImpl<T> *next = impl->next;
-      delete impl;
-      impl = next;
-    }
-  }
+  void clear();
+  
 };
 //Utility class iterator
 template <typename T> class ListIter {
   friend List<T>;
-  static void remove(List<T>& list, const T& t);
-  static void remove_all(List<T>& list, const T& t);
-    ListIter(const ListIter& ref)=delete;
+  static void remove(List<T>& list, const T& t);// Remove one specific element from the list with the value
+  static void remove_all(List<T>& list, const T& t);// Remove all elements with a given value from the list
+  // No constructors
+  ListIter(const ListIter& ref)=delete;
   ListIter( ListIter&& ref)=delete;
   ListIter& operator= (const ListIter& ref) =delete;
   ListIter& operator= (ListIter&& ref) =delete;
@@ -213,5 +127,113 @@ template <typename T> class ListIter {
 	}
       }
       impl = impl->next; 
+    }
+  }
+
+template <typename T> void List<T>::push_back(const T& value) // Add to the end
+  {
+    ListImpl<T>* currentBack = backImpl_;
+    if(listImpl_) {
+      backImpl_->next = new ListImpl<T>(value,currentBack);
+      backImpl_ = backImpl_->next;
+    } else {
+      listImpl_ = new ListImpl<T>(value);
+      frontImpl_ = listImpl_;
+      backImpl_ = listImpl_;
+    }
+    size_++;
+  }
+template <typename T> void List<T>::push_front(const T& value) 
+{ // Add to the front
+    ListImpl<T>* currentFront = frontImpl_;
+    if(listImpl_) {
+      frontImpl_->prev = new ListImpl<T>(value,0,currentFront);
+      frontImpl_ = frontImpl_->prev;
+      listImpl_ = frontImpl_;
+    } else {
+      listImpl_ = new ListImpl<T>(value);
+      frontImpl_ = listImpl_;
+      backImpl_ = listImpl_;
+    }
+    size_++;
+  }
+template <typename T>  void List<T>::pop_back(){ // remove last elem    
+    if( backImpl_) {
+      if( backImpl_->prev){
+	ListImpl<T>* tmpImpl = backImpl_->prev;
+	delete backImpl_;
+	backImpl_ = tmpImpl;
+	backImpl_->next = nullptr;
+	size_--;
+      } else if(size_ == 1) {
+	delete listImpl_;
+	frontImpl_ = backImpl_ = listImpl_ =nullptr;
+	size_ =0;
+      } else { // empty list. Do nothing.
+      }
+    }    
+  }
+  
+template <typename T> void List<T>::pop_front(){ // remove first elem
+    
+    if( frontImpl_) {
+      if( frontImpl_->next){
+	ListImpl<T>* tmpImpl = frontImpl_->next;
+	delete frontImpl_;
+	frontImpl_ = tmpImpl;
+	frontImpl_->prev = nullptr;
+	listImpl_= frontImpl_;
+	size_--;
+      } else if(size_ == 1) {
+	delete listImpl_;
+	frontImpl_ = backImpl_ = listImpl_ =nullptr;
+	size_ =0;
+      } else { // empty list. Do nothing.
+      }
+    }    
+
+  }
+template <typename T>  void List<T>::print(const char separator,std::ostream& out)const{
+    // Printing function with a single character separator 
+    ListImpl<T> *implPtr= listImpl_;
+    if(!implPtr){
+      out << endl;
+      return;
+    }
+    out << implPtr->value ;
+    implPtr = implPtr->next;
+    while(implPtr) {
+      out << separator <<implPtr->value ;
+      implPtr = implPtr->next;
+    }
+    out << std::endl;
+  }
+
+template <typename T>
+void List<T>::print(const char* separator,std::ostream &out)const{
+      // Printing function with a string separator 
+    ListImpl<T> *implPtr= listImpl_;
+    if(!implPtr){
+      out << endl;
+      return;
+    }
+    out << implPtr->value ;
+    implPtr = implPtr->next;
+    while(implPtr) {
+      out << separator <<implPtr->value ;
+      implPtr = implPtr->next;
+    }
+    out << std::endl;
+  }
+
+template <typename T>
+void List<T>::clear() { // Clears out all entries.
+    ListImpl<T>* impl= listImpl_;
+    size_ = 0;
+    frontImpl_ = backImpl_ = listImpl_ = nullptr;
+    while(impl) {
+      ListImpl<T> *next = impl->next;
+      delete impl;
+      impl = next;
     }
   }
--- StaxPayments/ListUT.cpp
+++ StaxPayments/ListUT.cpp
@@ -7,21 +7,24 @@ template <typename T> bool verifyCount(const List<T>& list , unsigned count )
 {
 
   bool result =(list.size() == count);
-  if(result) { cout << " Count TEST PASS " << endl;}
-  else  { cout << " COUNT TEST FAIL " << list.size()<< endl;}
+  cout << "Expected count " << count << endl;
+  if(result) { cout << "Count TEST PASS " << endl;}
+  else  { cout << "Count TEST FAIL " << list.size()<< endl;}
   return result;
 }
 
 template <typename T> bool verifyValue(const T& t , const T& expected )
 {
   bool result =(t == expected);
+  cout << "Expected value " << expected << endl;
   if(result) { cout << " Value TEST PASS " << endl;}
-  else  { cout << " Value TEST FAIL " << endl;}
+  else  { cout << " Value TEST FAIL " <<t << endl;}
   return result;
 }
 bool TestString()
 {
   List<string> strList;
+  cout << "Adding Months of the year" <<endl;
   strList.push_back("Jan");
   strList.push_back("Feb");
   strList.push_back("Mar");
@@ -39,18 +42,24 @@ bool TestString()
   strList.print();
   // Should show Jan to Dec
   strList.remove("Jun");
-  strList.remove("Jul");  
+  strList.remove("Jul");
+  cout << "Removed Jun and Jul" << endl;
   strList.print();
   // Jun and Jul gone
+  cout << "Removed Jan" << endl;
   strList.remove_all("Jan");
   strList.print();
-  // Jan Jun and Jul gone  
+  // Jan Jun and Jul gone
+  cout << "Removed Dec" << endl;
   strList.remove("Dec");
   strList.print();
   // Jan Jun Jul and Dec gone    
   bool verifiedCount1 = verifyCount(strList,8);
+  cout << "Checking first value " << endl;
   bool verifyValue1 = verifyValue(strList.front(),string("Feb"));
+  cout << "Checking last value " << endl;
   bool verifyValue2 = verifyValue(strList.back(),string("Nov"));
+  cout << "Clearing list " << endl;
   strList.clear();
   strList.print();
   bool verifyCount2 = verifyCount(strList,0);
@@ -70,29 +79,48 @@ bool TestUnsigned()
   for (unsigned i = 10; i>=1; i--) {
     uList.push_back(i);
   }
+  cout << "Initial List " << endl; 
   uList.print();
+  cout << "Removed 1 element value 10 " << endl;
   uList.remove(10);
+  uList.print();
+ 
   bool verifiedCount1 = verifyCount(uList,19);
   bool verifyValue1 = verifyValue(uList.front(),1U);
   bool verifyValue2 = verifyValue(uList.back(),1U);
-  uList.print();
+  cout << "Removed 1 element value 7 " << endl;
   uList.remove(7);
+  
+  uList.print();
   bool verifiedCount2 = verifyCount(uList,18);
+  cout << "Removed first element " << endl;
   uList.pop_front();
+  uList.print();
   bool verifyValue3 = verifyValue(uList.front(),2U);
   bool verifyValue4 = verifyValue(uList.back(),1U);
+  cout << " Removed all 4 and 5 " << endl;
   uList.remove_all(4);
   uList.remove_all(5);
+  cout << "Removed last element" << endl;
   uList.pop_back();
+  uList.print();
   bool verifiedCount3 = verifyCount(uList,12);
   uList.remove(1);
+  cout << "Remove 1 if it exists " <<endl;
   bool verifiedCount4 = verifyCount(uList,12);
   uList.print();
+  cout << "Clearing the list " << endl;
   uList.clear();
+  uList.print();
   bool verifyEmpty = uList.empty();
   if(verifyEmpty) cout << "Emptiness PASS" << endl;
   else
     cout << "Emptiness FAIL" << endl;
+  cout << "Adding value 100" << endl;
+  uList.push_front(100);
+  uList.print();
+  bool verifyValue5 = verifyValue(uList.front(),100U);
+  bool verifyValue6 = verifyValue(uList.back(),100U);
   return (verifiedCount1 &&
 	  verifiedCount2 &&
 	  verifiedCount3 &&
@@ -101,6 +129,8 @@ bool TestUnsigned()
 	  verifyValue2 &&
 	  verifyValue3 &&
 	  verifyValue4 &&
+	  verifyValue5 &&
+	  verifyValue6 &&
 	  verifyEmpty); 
 }
 
